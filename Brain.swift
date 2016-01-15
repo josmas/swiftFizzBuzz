b883d0113f567ea70ac2fe353145145531cb9985
josmas <josmasflores@gmail.com>
2016-01-15 20:49:16 +0000

    refactor divisible functions to partial application

--- Brain.swift
+++ Brain.swift
@@ -10,15 +10,19 @@ import Foundation
 
 class Brain: NSObject{
 
+    func isDivisibleBy(divisor :Int, number :Int) -> Bool {
+        return number % divisor == 0
+    }
+
     func isDivisibleByThree(number :Int) -> Bool {
-        return number % 3 == 0
+        return isDivisibleBy(3, number: number)
     }
 
     func isDivisibleByFive(number :Int) -> Bool {
-        return number % 5 == 0
+        return isDivisibleBy(5, number: number)
     }
 
     func isDivisibleByFifteen(number :Int) -> Bool {
-        return isDivisibleByFive(number) && isDivisibleByFive(number)
+        return isDivisibleByThree(number) && isDivisibleByFive(number)
     }
 }
